b6735a7873be878c19c257bcabbebee6753ab2b6
Márk Tajti <mark.tajti@tesco.com>
2025-07-09 16:30:13 +0200

    Init project

---
+++ BundlerSizer/BundlerSizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BundlerSizerApp.swift
+//  BundlerSizer
+//
+//  Created by Márk Tajti on 2025. 07. 09..
+//
+
+import SwiftUI
+
+@main
+struct BundlerSizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tuist.swift
@@ -0,0 +1,3 @@
+import ProjectDescription
+
+let tuist = Tuist(fullHandle: "tesco/BundlerSizer", project: .xcode())
