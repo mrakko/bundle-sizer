3147a13f466670b3f7fde573134079a3b20247e3
Márk Tajti <mark.tajti@tesco.com>
2025-07-11 10:02:54 +0200

    Add image

--- BundlerSizer/ContentView.swift
+++ BundlerSizer/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Image("tree")
+                .resizable()
         }
         .padding()
     }
